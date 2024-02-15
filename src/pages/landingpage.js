fc6f268ccdeb0a8bda8a20e2e782d2ad9427a275
Divij <ndivij2004@gmail.com>
2024-02-15 23:30:43 +0530

    first commit

---
+++ src/pages/landingpage.js
@@ -0,0 +1,16 @@
+import Nav1 from "../components/Navbar"
+import Footer from "../components/Footer"
+import Quotes from "../components/quotes"
+import Hero from "../components/Hero"
+function Land(){
+    return(
+        <div>
+            <Nav1/>
+            <Hero/>
+            <Quotes/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Land;
